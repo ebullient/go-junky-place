08b5ac56f2ddc3c4b18eae16d5c9366e23a72c70
Erin Schnabel <ebullient@redhat.com>
2020-09-22 21:56:01 -0400

    fix

--- server-start.sh
+++ server-start.sh
@@ -2,6 +2,6 @@
 
 export JAVA_LIB_DIR=target/lib
 export JAVA_APP_DIR=target
-export JAVA_APP_JAR=$(cd target; ls junky*-runner.jar)
+export JAVA_APP_JAR=junky-place-1.0-SNAPSHOT-runner.jar
 export JAVA_OPTIONS="-Dquarkus.http.host=0.0.0.0 -Djava.util.logging.manager=org.jboss.logmanager.LogManager -Dquarkus.http.port=${PORT}"
 ./run-java.sh
