c47df9d75095dadb60ce275fa2442f00b9386761
Erin Schnabel <ebullient@redhat.com>
2020-09-22 22:07:26 -0400

    heroku launch

--- server-start.sh
+++ server-start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-export JAVA_LIB_DIR=target/lib
-export JAVA_APP_DIR=target
-export JAVA_APP_JAR=junky-place-1.0-SNAPSHOT-runner.jar
-export JAVA_OPTIONS="-Dquarkus.http.host=0.0.0.0 -Djava.util.logging.manager=org.jboss.logmanager.LogManager -Dquarkus.http.port=${PORT}"
-./run-java.sh
+java \
+  -Dquarkus.http.host=0.0.0.0 \
+  -Dquarkus.http.port=${PORT} \
+  -Djava.util.logging.manager=org.jboss.logmanager.LogManager \
+  -cp  target/lib \
+  -jar target/junky-place-1.0-SNAPSHOT-runner.jar
