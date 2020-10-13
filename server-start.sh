72ae0096f50d40b0504f5ebebc235dfa3a8225d8
Erin Schnabel <ebullient@redhat.com>
2020-10-13 16:24:56 -0400

    remove heroku

--- server-start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-java \
-  -Dquarkus.http.host=0.0.0.0 \
-  -Dquarkus.http.port=${PORT} \
-  -Djava.util.logging.manager=org.jboss.logmanager.LogManager \
-  -cp  target/lib \
-  -jar target/junky-place-1.0-SNAPSHOT-runner.jar
